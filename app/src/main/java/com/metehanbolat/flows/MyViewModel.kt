72eda5b06d5e62b33051c1f2c8b5fa2e6aacdd4d
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-01-18 22:43:39 +0300

    filter - map - collect operators

--- app/src/main/java/com/metehanbolat/flows/MyViewModel.kt
+++ app/src/main/java/com/metehanbolat/flows/MyViewModel.kt
@@ -4,7 +4,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
 class MyViewModel : ViewModel() {
@@ -29,8 +31,17 @@ class MyViewModel : ViewModel() {
 
     private fun collectInViewModel() {
 
+        // collect verileri toplama işlemi yaptığımız yer yani önce 3 ile bölünenleri filtreleyip ardından collect ile topluyoruz
         viewModelScope.launch {
-            countDownTimerFlow.collect {
+            countDownTimerFlow
+                .filter {
+                    it %3 == 0
+                }
+                // Filtreleme işlemi yaptıktan sonra mapping yaparak farklı değerler elde edebiliriz.
+                .map {
+                    it + it
+                }
+                .collect {
                 println("Counter is: $it")
             }
         }
