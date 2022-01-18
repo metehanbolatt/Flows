7c655b212f3203f86dd021f374e210808dac1f3a
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-01-18 22:31:49 +0300

    CountDownTimer with flow(collectAsState)

--- app/src/main/java/com/metehanbolat/flows/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/flows/MainActivity.kt
@@ -3,8 +3,17 @@ package com.metehanbolat.flows
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 import com.metehanbolat.flows.ui.theme.FlowsTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,8 +21,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FlowsTheme {
+                
+                val viewModel : MyViewModel by viewModels()
+                val counter = viewModel.countDownTimerFlow.collectAsState(initial = 10)
                 Surface(color = MaterialTheme.colors.background) {
-
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                    ){
+                        Text(
+                            modifier = Modifier.align(Alignment.Center),
+                            text = counter.value.toString(),
+                            fontSize = 26.sp,
+                            textAlign = TextAlign.Start,
+                        )
+                    }
                 }
             }
         }
--- app/src/main/java/com/metehanbolat/flows/MyViewModel.kt
+++ app/src/main/java/com/metehanbolat/flows/MyViewModel.kt
@@ -1,7 +1,22 @@
 package com.metehanbolat.flows
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
 
 class MyViewModel : ViewModel() {
-    
+
+    // Flow içerisinde yayılacak ve toplanacak verilerin tipini yazıyoruz.
+    // Yapılacak işleri kod bloğunda yazıyoruz.
+    // Emit demek veriyi yaymak demek.
+    val countDownTimerFlow = flow<Int> {
+        val countDownFrom = 10
+        var counter = countDownFrom
+        emit(countDownFrom)
+        while (counter > 0){
+            delay(1000)
+            counter--
+            emit(counter)
+        }
+    }
 }
